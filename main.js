175694a8b8acab9e81f805a60ac245b575f2cbcc
unknown <kheronn@gmail.com>
2024-04-03 16:45:40 -0300

    R05 - Converter data em unidades de medidas de tempo

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 //Percorrer (iterar) uma lista de elementos
-for(let i = 0; i < botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j = 0; j < botoes.length; j++){
-               botoes[j].classList.remove("ativo");
-               textos[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
     }
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  };
 }
 // document.querySelector("#minhaDiv p").textContent = "Chega de calor";
 const contadores = document.querySelectorAll(".contador");
 let tempoAtual = new Date();
-const tempoObjetivo1 = new Date("2024-10-20T00:00:00");
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempoObjetivo1 = new Date("2024-05-03T00:00:00");
+
+function calculaTempo(tempoObjetivo) {
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
