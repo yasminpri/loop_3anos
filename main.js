05cbbe8cf687b1b9fe76474beca6a5bc826f3c65
Miderson Santana <103211766+Miderson@users.noreply.github.com>
2024-04-14 20:52:44 -0300

    furacão

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function verificarTime() {
-    // Variável para armazenar a resposta do usuário
+
     var respostaTime = prompt("Qual o melhor time?");
   
-    // Verificar se a resposta é "Athlético PR"
+    
     if (respostaTime.toLowerCase() === "Athlético PR") {
       alert("Isso mesmo! O Athlético PR é o melhor time!");
     } else {
