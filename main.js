adaa507c79e9f6d9d3f9685ea7be9b0b4d8e5cb9
Miderson Santana <103211766+Miderson@users.noreply.github.com>
2024-04-14 21:02:04 -0300

    res

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-function verificarTime() {
 
-    var respostaTime = prompt("Qual o melhor time?");
+function verificarTime() {
   
+  var continuarPerguntando = true;
+
+  while (continuarPerguntando) {
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
+    var respostaTime = prompt("Qual o melhor time?");
+
+    
+    if (respostaTime.toLowerCase() === "athlético pr") {
       alert("Isso mesmo! O Athlético PR é o melhor time!");
+      continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
     }
   }
-  
-  // Chamada da função para iniciar o processo
-  verificarTime();
+}
+
+// Chamada da função para iniciar o processo
+verificarTime();
+
   
