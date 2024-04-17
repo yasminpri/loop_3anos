347a2da0282b940571de7692889194b78b2c49ca
Miderson Santana <103211766+Miderson@users.noreply.github.com>
2024-04-17 13:44:32 -0300

    vermelho

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function verificarTime() {
     var respostaTime = prompt("Qual os melhor time?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
+    if (respostaTime.toLowerCase() === "Athlético PR") {
       alert("Isso mesmo! O Athlético PR é o melhor time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
